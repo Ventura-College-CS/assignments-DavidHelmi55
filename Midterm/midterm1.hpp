ddf8e8a3d002df7e8dbc6364ef285e783f0cf6a7
DavidHelmi55 <77356034+DavidHelmi55@users.noreply.github.com>
2021-11-26 11:16:23 -0800

    .

---
+++ Midterm/midterm1.hpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <class T>
+class Stack
+{
+
+    private:
+        vector<T> p;
+    
+    public:
+        stack();
+
+        void push(T el)
+        {
+            
+        }
+
+};
