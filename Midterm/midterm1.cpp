274f7054470890fd7cd32a74518902e19e935e6c
DavidHelmi55 <77356034+DavidHelmi55@users.noreply.github.com>
2021-11-26 11:56:14 -0800

    .

--- Midterm/midterm1.cpp
+++ Midterm/midterm1.cpp
@@ -5,10 +5,11 @@ using namespace std;
 int main()
 {
 
-    Student(1001, "John", 100, 20, 100);
-    Student(1002, "Bill", 5, 30, 40);
-    Student(1003, "Nathan", 60, 70, 75);
-    Student(1004, "David", 100, 100, 100);
-    Student(1005, "Mark", 100, 40, 100);
+    Student1(1001, "John", 100, 20, 100);
+    Student2(1002, "Bill", 5, 30, 40);
+    Student3(1003, "Nathan", 60, 70, 75);
+    Student4(1004, "David", 100, 100, 100);
+    Student5(1005, "Mark", 100, 40, 100);
 
+    
 }
