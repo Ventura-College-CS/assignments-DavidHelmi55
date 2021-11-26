a8402a86182c118775d9a528bf3f7673eea69eb4
DavidHelmi55 <77356034+DavidHelmi55@users.noreply.github.com>
2021-11-26 11:38:03 -0800

    .

--- Midterm/midterm1.hpp
+++ Midterm/midterm1.hpp
@@ -1,8 +1,37 @@
+#ifndef STACK_H
+#define STACK_H
 #include <iostream>
 #include <vector>
 using namespace std;
 
 template <class T>
+
+class Student
+{
+
+    private:
+        int SID;
+        string SName;
+        double scores [3];
+
+    public:
+        Student();
+        Student(int i, string name, double *sc);
+        int getID()
+        {
+            
+        }
+        string getName()
+        {
+
+        }
+        double *getScores()
+        {
+
+        }
+
+};
+
 class Stack
 {
 
@@ -10,40 +39,32 @@ class Stack
         vector<T> p;
     
     public:
-        stack();
-
+        stack(int size)
+        {
+            p.reserve(size);
+        }
         void push(T el)
         {
-
-
+            p.push_back(el);
         }
-
         T pop()
         {
-
-            
-
+            T val = p.back();
+            p.pop_back();
+            return val;
         }
-
         T &topEl()
         {
-
             return p.back();
-
         }
-
         int isEmpty()
         {
-
             return p.empty();
-
         }
-
         int size()
         {
-
             return p.size();
-
         }
-
 };
+
+#endif
