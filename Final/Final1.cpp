41b5c679f3a6cfda2d7628d1e625505796f6cd6f
DavidHelmi55 <77356034+DavidHelmi55@users.noreply.github.com>
2021-12-08 17:23:21 -0800

    .

--- Final/Final1.cpp
+++ Final/Final1.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
+
+struct Class{
+    int CourseID;
+    string CourseName;
+    int CourseCredit;
+};
 
 int binarySearch(int array[], int first, int last, int target)
 {
@@ -19,14 +26,24 @@ int binarySearch(int array[], int first, int last, int target)
 
 int main()
 {
+        int target;
+        ifstream ifs;
+        ifs.open("class.txt");
+	if ( ifs.fail())
+	{
+		cerr << "File Open Error\n";
+		exit(0);
+	}
+
+        cout << "Which CourseID would you like information for? " << endl;
+        cin >> target;
     
-    int array[] = {10 , 15, 20, 25, 30, 35, 40, 45, 50, 55};
-    int target = 25;
-    int x = sizeof(array) / sizeof(array[0]);
-    int result = binarySearch(array, 0, x-1, target);
-    (result == -1)
-        ? cout << "Element is not present in array"
-        : cout << "Element is present at index " << result;
-    return 0;
+        int array[] = {10 , 15, 20, 25, 30, 35, 40, 45, 50, 55};
+        int x = sizeof(array) / sizeof(array[0]);
+        int result = binarySearch(array, 0, x-1, target);
+        (result == -1)
+                ? cout << "Element is not present in array"
+                : cout << "Element is present at index " << result;
+        return 0;
     
 }
