d7eb273f884b8dbbccfdcd2e9b8023dc88956fea
DavidHelmi55 <77356034+DavidHelmi55@users.noreply.github.com>
2021-12-13 11:19:15 -0800

    .

---
+++ Final/Final1/Final1.hpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Class
+{
+
+    private:
+        int CourseID;
+        string CourseName;
+        int CourseCredit;
+
+    public:
+        Class() : CourseID(0), CourseName() {}
+        Class(int i, string name, int x)
+        {
+            CourseID = i;
+            CourseName = name;
+            CourseCredit = x;
+        }
+        int getID() const{return CourseID;}
+        string getName() const { return CourseName;}
+        int const &getCredit() const { return CourseCredit; }
+	    void setSid(int id) { CourseID = id; }
+	    void setSname(string name) { CourseName = name; }
+        void setCredit(int s) {CourseCredit = s; }
+	    void printClass() const
+	    {
+		    cout << "Class ID: " << CourseID << "\t";
+		    cout << "Course Name: " << CourseName << "\t";
+		    cout << "Course Credit: " << CourseCredit << "\t";
+		    cout << endl;
+	    }
+
+};
